bcacd773f6d8786e5a5e5ed4f1959a42e17721dd
rayhan <prog.rayhan1@gmail.com>
2023-04-01 23:31:30 +0600

    topic: 2. O(n) problem solving
    1.Big O(n) 2nd problem solved

---
+++ DSA/Big O/JS/07-Big_O_Problem-2.js
@@ -0,0 +1,23 @@
+function anotherFunChallenge(input){
+    let a = 5; // O(1)
+    let b = 10; // O(1)
+    let c = 30; // O(1)
+
+    for(let i=0; i<input.length; i++){ // O(n)
+        let x = i+1; // O(n)
+        let y = i+2; // O(n)
+        let z = i+3; // O(n)
+    }
+
+    for (let p=0; p<input; p++){ // O(n)
+        let e = p*2; // O(n)
+        let f = p*4; // O(n)
+    }
+
+    let myString = "I Love DSA" // O(1)
+}
+
+// O(1)+O(1)+O(1)+O(n)+O(n)+O(n)+O(n)+O(n)+O(n)+O(n)+O(1)
+// O(4)+O(7) = O(4+7n) = O(n)
+
+anotherFunChallenge() // O(n)
---
+++ DSA/Big O/Python/07-Big_O_Problem-2.py
@@ -0,0 +1,17 @@
+def anotherFunChallenge(input):
+    a = 5 # O(1)
+    b = 10 # O(1)
+    c = 15 # O(1)
+
+    for i in range(input): # O(n)
+        p = i+1 # O(n)
+        q = i+2 # O(n)
+        r = i+3 # O(n)
+
+    for x in range(input): # O(n)
+        print(f"Hello {x}") # O(n)
+        h = x*2 # O(n)
+
+    myString = "This is a string" # O(1)
+
+anotherFunChallenge() # O(n)
